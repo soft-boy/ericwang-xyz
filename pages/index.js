f7cc9bcaf31bbb8a77b5b1f75436ab9572bf44dd
Eric Wang <ewang1997@gmail.com>
2026-08-01 11:45:02 -0700

    remove eric mail finder

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,6 @@ export default function Home() {
         </div>
         <div className="category">work</div>
         <ul>
-          <li><a href="https://github.com/soft-boy/ericmailfinder">ericmailfinder</a> - wip</li>
           <li><a href="https://ewurl.cc/">ew-url</a> - tinyurl, but made by me</li>
           <li><a href="https://www.datamonster.me/">datamonster.me</a> - generate datamonsters</li>
           <li><a href="https://github.com/soft-boy/breakout-dqn">breakout-dqn</a> - deep q-learning for breakout</li>
